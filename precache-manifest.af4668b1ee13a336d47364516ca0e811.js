172850fe60237d48a3a7fe6f5f647a00084330b7
Zachary Li Norman <zacharylinorman@gmail.com>
2020-11-20 15:09:56 -0800

    Updates

---
+++ precache-manifest.af4668b1ee13a336d47364516ca0e811.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e228712717c5b05acf5c1f32ee6d7bcd",
+    "url": "/boostingsite/index.html"
+  },
+  {
+    "revision": "8c8dd8b96e2796ac728e",
+    "url": "/boostingsite/static/css/main.c975af1e.chunk.css"
+  },
+  {
+    "revision": "1a4717c18366cb04e7f5",
+    "url": "/boostingsite/static/js/2.c0689d24.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/boostingsite/static/js/2.c0689d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c8dd8b96e2796ac728e",
+    "url": "/boostingsite/static/js/main.751cf4df.chunk.js"
+  },
+  {
+    "revision": "7fd6e58aec3faf8f19b4",
+    "url": "/boostingsite/static/js/runtime-main.0aec8149.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/boostingsite/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "60a07ea01e44969b7aab5301abdc9558",
+    "url": "/boostingsite/static/media/red_vs_blue_edited.60a07ea0.jpg"
+  }
+]);
